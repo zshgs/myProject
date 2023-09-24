fafe1747ee3f6615121af882bdf7849efa998fea
zshgs <qq1044559054@gmail.com>
2023-09-24 16:15:59 +1000

    Added myProject

---
+++ myDemo/src/test/java/com/example/mydemo/MyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
